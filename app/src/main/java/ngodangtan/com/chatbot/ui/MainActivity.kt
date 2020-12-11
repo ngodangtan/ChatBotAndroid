94b7a490613e060bde1719bb19bc6d4cf6d47582
27293 <myltd6@fpt.com.vn>
2020-12-11 14:56:29 +0700

    done

--- app/src/main/java/ngodangtan/com/chatbot/ui/MainActivity.kt
+++ app/src/main/java/ngodangtan/com/chatbot/ui/MainActivity.kt
@@ -1,14 +1,126 @@
 package ngodangtan.com.chatbot.ui
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 import ngodangtan.com.chatbot.R
+import ngodangtan.com.chatbot.data.Message
+import ngodangtan.com.chatbot.utils.BotResponse
+import ngodangtan.com.chatbot.utils.Constants.OPEN_GOOGLE
+import ngodangtan.com.chatbot.utils.Constants.OPEN_SEARCH
+import ngodangtan.com.chatbot.utils.Constants.RECEIVE_ID
+import ngodangtan.com.chatbot.utils.Constants.SEND_ID
+import ngodangtan.com.chatbot.utils.Time
 
 class MainActivity: AppCompatActivity() {
+
+    private lateinit var adapter: MessagingAdapter
+    private val botList = listOf("Peter","Francesca","Luigi","Igor")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView()
+
+        clickEvents()
+
+        val random = (0..3).random()
+
+        customMessage("Hello! Today you're speaking with ${botList[random]}, how may i help?")
+
+
+    }
+
+    private fun clickEvents(){
+        btn_send.setOnClickListener {
+            sendMessage()
+        }
+        et_message.setOnClickListener {
+            GlobalScope.launch {
+                delay(100)
+                withContext(Dispatchers.Main){
+                    rv_messages.scrollToPosition(adapter.itemCount-1)
+                }
+            }
+        }
+    }
+
+    private fun recyclerView(){
+        adapter = MessagingAdapter()
+        rv_messages.adapter = adapter
+        rv_messages.layoutManager = LinearLayoutManager(applicationContext)
+    }
+
+    private fun sendMessage(){
+        val message = et_message.text.toString()
+        val timeStamp = Time.timeStamp()
+
+        if (message.isNotEmpty()){
+            et_message.setText("")
+
+            adapter.insertMessage(Message(message,SEND_ID,timeStamp))
+            rv_messages.scrollToPosition(adapter.itemCount-1)
+
+            botResponse(message)
+        }
+    }
+
+    private fun botResponse(message: String){
+        val timeStamp = Time.timeStamp()
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                val response = BotResponse.basicResponses(message)
+
+                adapter.insertMessage(Message(response, RECEIVE_ID,timeStamp))
+                rv_messages.scrollToPosition(adapter.itemCount-1)
+
+                when(response){
+                    OPEN_GOOGLE -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        site.data = Uri.parse("https://www.google.com/")
+                        startActivity(site)
+                    }
+                    OPEN_SEARCH -> {
+                        val site = Intent(Intent.ACTION_VIEW)
+                        val searchTerm: String? = message.substringAfter("search")
+                        site.data = Uri.parse("https://www.google.com/search?&q=$searchTerm")
+                        startActivity(site)
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                rv_messages.scrollToPosition(adapter.itemCount-1)
+            }
+        }
+    }
+
+    private fun customMessage(message:String){
+        GlobalScope.launch {
+            delay(1000)
+            withContext(Dispatchers.Main){
+                val timeStamp = Time.timeStamp()
+                adapter.insertMessage(Message(message,RECEIVE_ID,timeStamp))
+
+                rv_messages.scrollToPosition(adapter.itemCount-1)
+            }
+        }
 
     }
 }
